87fec035e23d0ff129787a75b076b47b58ee5830
chriopter <ce@mail101.de>
2025-03-02 01:10:29 +0100

    Small commit

--- setup.sh
+++ setup.sh
@@ -11,20 +11,19 @@ mkdir -p .github/workflows
 # Copy .hugoversion file if it doesn't exist
 if [ ! -f .hugoversion ]; then
   echo "Creating .hugoversion file..."
-  echo "0.123.8" > .hugoversion
-  echo "Created .hugoversion with default Hugo version 0.123.8"
-  echo "You may want to update this to your preferred Hugo version"
+  curl -s -o .hugoversion https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/example-user-repo/.hugoversion
+  echo "Created .hugoversion file"
 fi
 
 # Download dependabot.yml
 echo "Downloading dependabot.yml..."
-curl -s -o .github/dependabot.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/.github/dependabot.yml
+curl -s -o .github/dependabot.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/example-user-repo/.github/dependabot.yml
 
 # Download workflow files
 echo "Downloading workflow files..."
-curl -s -o .github/workflows/hugo-autopilot-builder.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/.github/workflows/hugo-autopilot-builder.yml
-curl -s -o .github/workflows/hugo-autopilot-updater.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/.github/workflows/hugo-autopilot-updater.yml
-curl -s -o .github/workflows/hugo-autopilot-dependabot-merger.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/.github/workflows/hugo-autopilot-dependabot-merger.yml
+curl -s -o .github/workflows/hugo-autopilot-builder.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/example-user-repo/.github/workflows/hugo-autopilot-builder.yml
+curl -s -o .github/workflows/hugo-autopilot-updater.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/example-user-repo/.github/workflows/hugo-autopilot-updater.yml
+curl -s -o .github/workflows/hugo-autopilot-dependabot-merger.yml https://raw.githubusercontent.com/chriopter/hugo-autopilot/main/example-user-repo/.github/workflows/hugo-autopilot-dependabot-merger.yml
 
 echo "Setup complete!"
 echo "Don't forget to configure GitHub repository settings:"
